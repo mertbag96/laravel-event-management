821e040d515cfc8eb8b2f06865db5d8df30c0cc0
Mert Bağ <bagmert1996@gmail.com>
2025-05-31 06:43:32 +0300

    feat: replace default welcome page with homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Event Management</title>
+
+        <style>
+            body {
+                background-color: #111;
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100dvh;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-family: 'Lucida Sans', sans-serif;
+                font-weight: bold;
+                font-size: 2rem;
+                color: #777;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Laravel Event Management</h1>
+    </body>
+</html>
