ad9b87bd3b4ece49f7fbb2fb9d46ea8ea24aa24f
Mert Bağ <bagmert1996@gmail.com>
2025-05-31 19:38:30 +0300

    feat(attendee): implement CRUD operations and resources for API

--- app/Http/Controllers/AttendeeAPIController.php
+++ app/Http/Controllers/AttendeeAPIController.php
@@ -2,49 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Attendee;
+
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 use App\Http\Controllers\Controller;
+
+use App\Http\Resources\AttendeeResource;
 
 class AttendeeAPIController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event)
     {
-        //
+        $attendees = $event->attendees()->get();
+
+        return AttendeeResource::collection($attendees);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
-        //
+        $attendee = $event->attendees()->create([
+            'user_id' => 1,
+        ]);
+
+        return new AttendeeResource($attendee);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event, Attendee $attendee)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
+        return new AttendeeResource($attendee);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $event, Attendee $attendee): Response
     {
-        //
+        $attendee->delete();
+
+        return response(status: 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\EventAPIController;
 use App\Http\Controllers\AttendeeAPIController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::apiResource('events', EventAPIController::class);
 
-Route::apiResource('events.attendees', AttendeeAPIController::class)
-    ->scoped(['attendee' => 'event']);
+Route::apiResource('events.attendees', AttendeeAPIController::class)->scoped()->except(['update']);
