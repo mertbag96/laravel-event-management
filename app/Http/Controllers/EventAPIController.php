93b7cac7b104638384f1503b2b24c9a1e5f8ad0c
Mert Bağ <bagmert1996@gmail.com>
2025-05-31 06:33:40 +0300

    feat(event): implement CRUD operations for API

--- app/Http/Controllers/EventAPIController.php
+++ app/Http/Controllers/EventAPIController.php
@@ -2,49 +2,68 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Event;
+
+use Illuminate\Http\Response;
+
+use Illuminate\Support\Collection;
 
 use App\Http\Controllers\Controller;
+
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
 
 class EventAPIController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @return Collection<Event>
      */
-    public function index()
+    public function index(): Collection
     {
-        //
+        return Event::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreEventRequest $request): Event
     {
-        //
+        $event = Event::create([
+            'user_id' => 1,
+            ...$request->validated()
+        ]);
+
+        return $event;
     }
 
     /**
      * Display the specified resource.
+     * @param Event $event
+     * @return Event
      */
-    public function show(string $id)
+    public function show(Event $event): Event
     {
-        //
+        return $event;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateEventRequest $request, Event $event): Event
     {
-        //
+        $event->update($request->validated());
+
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event): Response
     {
-        //
+        $event->delete();
+
+        return response(status: 204);
     }
 }
